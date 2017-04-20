6e017258913e5be60c265e23b3123c5fe92c55de
Ryan Wade <ryan.wade@protonmail.com>
2017-04-20 16:39:00 +0800

    Bind to localhost

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
 threads_count = ENV.fetch('MAX_THREADS') { 5 }.to_i
 threads threads_count, threads_count
 
-port        ENV.fetch('PORT') { 3000 }
+bind        "tcp://127.0.0.1:#{ENV.fetch('PORT') { 3000 }}"
 environment ENV.fetch('RAILS_ENV') { 'development' }
 workers     ENV.fetch('WEB_CONCURRENCY') { 2 }
 
